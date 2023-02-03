4e10ae60f9efba5ca5e787932ba29e6bb6a18580
koi <koi@atelier.my.domain>
2023-02-03 12:20:37 -0500

    clean up transpiler.py

--- proof_of_concept/mc_to_ly/transpiler.py
+++ proof_of_concept/mc_to_ly/transpiler.py
@@ -31,44 +31,6 @@ PITCH2INT = {
     'B': 6
 }
 
-# Extracts value from node 'node' in the lark tree 
-def extract_node_value(node):
-    values = node.pretty().split()[1:]
-    return ' '.join(values)
-
-# Extracts information from lark tree and returns dictionary containing stored info 
-def extract_score_info(tree):
-    result = dict() 
-
-    # Get score metadata (title, composer)
-    # TODO: Ensure that only one title and one composer is provided 
-    for title in tree.find_data("title"):
-        result['title'] = extract_node_value(title)
-    for composer in tree.find_data('composer'):
-        result['composer'] = extract_node_value(composer)
-
-    # Get all notes. TODO: Handle more than one part 
-    notes_list = list()
-    all_part_nodes = tree.find_data("part") # Extract iterable of all parts 
-    for part_node in all_part_nodes:
-        note_nodes = part_node.find_data("note") # Extract iterable of all notes within current part 
-        for note_node in note_nodes:
-            note_map = dict()
-            # Each 'for' loop below goes through a one-element iterable
-            for pitch_node in note_node.find_data("pitch"):
-                pitch = extract_node_value(pitch_node)
-                note_map['pitch'] = pitch 
-            for octave_node in note_node.find_data("octave"):
-                octave = int(extract_node_value(octave_node))
-                note_map['octave'] = octave 
-            for duration_node in note_node.find_data("duration"):
-                duration = int(extract_node_value(duration_node))
-                note_map['duration'] = duration 
-            notes_list.append(note_map)
-    result['notes'] = notes_list 
-
-    return result 
-
 
 # Converter functions converting between values in naive AST and the integer 
 # representation that would be passed into the ly library function. 
@@ -81,32 +43,10 @@ def octave2int(o):
 def duration2int(d):
     return int(math.log(d, 2))
 
-# Add note 'note' to the sequence 'parent'
-def add_note(note, parent):
-    pitch = dom.Pitch(octave=octave2int(note['octave']),
-                      note=pitch2int(note['pitch']),
-                      parent=parent)
-    duration = dom.Duration(duration2int(note['duration']), parent=parent)
-
 # Add double bar line to the sequence 'parent' 
-def add_double_barline(parent):
-    barline_cmd = dom.Command("bar", parent=parent)
-    barline_txt = dom.Text("\"|.\"", parent=barline_cmd)
-
-# Print lilypond code for score with info contained in 'ast' 
-def print_score(ast):
-    header = dom.Header();
-    header['title'] = ast['title']
-    header['composer'] = ast['composer']
-
-    score = dom.Score()
-    notes = dom.Seq(parent=score)
-    for note in ast['notes']:
-        add_note(note, notes)
-    add_double_barline(notes)
-    
-    print(header.ly(dom.Printer()))
-    print(score.ly(dom.Printer()))
+# def add_double_barline(parent):
+#     barline_cmd = dom.Command("bar", parent=parent)
+#     barline_txt = dom.Text("\"|.\"", parent=barline_cmd)
 
 
 
@@ -177,7 +117,7 @@ class MyTransformer(Transformer):
 
     def start(self, args):
         flat_args = flatten(args)
-        print(flat_args)
+        # print(flat_args)
         dict_args = dict(flat_args)
         return mc_ast.Score(dict_args, dict_args['part'])
         
@@ -205,10 +145,7 @@ def main():
     print(hdr.ly(p))
     print(score.ly(p))
 
-    # score_info = extract_score_info(tree)
-
-    # print_score(score_info)
-
+    
 if __name__ == '__main__':
     main()
 
